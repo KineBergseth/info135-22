c127130e43b03076f6f15a59751e1788739526ad
KineBergseth <kinber96@gmail.com>
2022-03-29 10:02:37 +0200

    lab10

--- oblig4.py
+++ oblig4.py
@@ -1,76 +1,142 @@
-"""
-There are N queens on a chess board of NxN
-How can they be positioned so that none of them attacks another according to the rules of chess:
-2 queens can not be on:
-the same row
-the same column
-the same diagonal
+#Oblig 5
+#Oppgave 1
+#d) alle tre er fulle binary threes ettersom alle nodene har 2 eller 0 barn
+#Oppgave 2
 
-examine partial solujtion:
-if 2 queens attack each other, reject it
-if it has N=4 queens, accept as solution
-otherwise contine
+def binary_tree(r):
+    return [r, [], []]
 
-"""
-# N = 5
-COLUMNS = 'abcde'
-NUM_QUEENS = len(COLUMNS)
-ACCEPT = 1
-CONTINUE = 2
-ABANDON = 3
-all_solutions = []
+def get_left_child(root):
+    return root[1]
 
+def get_right_child(root):
+    return root[2]
 
-def extend(partial_sol):
-    results = []
-    row = len(partial_sol) + 1
-
-    for column in COLUMNS:
-        new_solution = list(partial_sol)
-        new_solution.append(column + str(row))
-        results.append(new_solution)
-    return results
-
-
-def examine(partial_sol):
-    for i in range(len(partial_sol)):
-        for j in range(i + 1, len(partial_sol)):
-            if attacks(partial_sol[i],partial_sol[j]):
-                return ABANDON
-    if len(partial_sol) == NUM_QUEENS:
-        return ACCEPT
+def insert_left_child(root, new_branch):
+    t = root.pop(1)
+    if len(t) > 1:
+        root.insert(1, [new_branch, t, []])
     else:
-        return CONTINUE
+        root.insert(1, [new_branch, [], []])
+    return root
+
+def insert_right_child(root, new_branch):
+    t = root.pop(2)
+    if len(t) > 1:
+        root.insert(2, [new_branch, [], t])
+    else:
+        root.insert(2, [new_branch, [], []])
+    return root
+
+def make_tree():
+    my_tree = binary_tree('1')
+
+    insert_left_child(my_tree, '2')
+    insert_right_child(my_tree, '3')
+
+    insert_left_child(get_left_child(my_tree), '4')
+    insert_left_child(get_right_child(my_tree), '5')
+
+    insert_right_child(get_right_child(my_tree), '6')
+
+    print(my_tree)
+
+make_tree()
+
+print()
+#Oppgave 3
+class Graph:
+    graph = dict()
+    searched = []
+
+    def add_edge(self, node, neighbour):
+        if node not in self.graph:
+            self.graph[node] = [neighbour]
+        else:
+            self.graph[node].append(neighbour)
+
+    def print_graph(self):
+        print(self.graph)
+
+    def d_f_s(self, node):
+        if node not in self.searched:
+            print('[', node, end= '],')
+            self.searched.append(node)
+            if node in self.graph:
+                for neighbour in self.graph[node]:
+                    self.d_f_s(neighbour)
+
+def build_my_graph2():
+    my_graph = Graph()
+    my_graph.add_edge('a', 'b')
+    my_graph.add_edge('a', 'c')
+    my_graph.add_edge('b', 'c')
+    my_graph.add_edge('b', 'c')
+    my_graph.add_edge('b', 'd')
+    my_graph.add_edge('c', 'e')
+    my_graph.add_edge('d', 'e')
+    my_graph.add_edge('d', 'g')
+    my_graph.add_edge('d', 'h')
+    my_graph.add_edge('e', 'f')
+    my_graph.add_edge('f', 'c')
+
+    my_graph.d_f_s('a')
+
+build_my_graph2()
+print()
+#Oppgave 4
+
+class BinarySearchTree:
+    def __init__(self, value=None):
+        self.value = value
+        if self.value:
+            self.left_child = BinaraySeachTree()
+            self.right_child = BinaraySeachTree()
+        else:
+            self.left_child = None
+            self.right_child = None
+
+    def is_empty(self):
+        return self.value is None
+
+    def insert(self, value):
+        if self.is_empty():
+            self.value = value
+            self.left_child = BinarySearchTree()
+            self.right_child = BinarySearchTree()
+
+        elif value < self.value:
+            self.left_child.insert(value)
+
+        elif value > self.value:
+            self.right_child.insert(value)
+
+    def in_order(self):
+        if self.is_empty():
+            return []
+        else:
+            return self.left_child.in_order() + [self.value] + self.right_child.in_order()
+
+    def compute_sum(self):
+        summen = 0
+        for x in self.in_order():
+            summen += x
+        return summen
+
+    def compute_count(self):
+        count = len(self.in_order())
+        return count
 
 
-def attacks(p1, p2):
-    column1 = COLUMNS.index(p1[0]) + 1
-    row1 = int(p1[1])
-    column2 = COLUMNS.index(p2[0]) + 1
-    row2 = int(p2[1])
-    return row1 == row2 or column1 == column2 or abs(row1 - row2) == abs(column1 - column2)
+
+my_tree = BinarySearchTree()
+my_tree.insert(2)
+my_tree.insert(4)
+my_tree.insert(6)
+my_tree.insert(8)
+my_tree.insert(10)
+print('sum:', my_tree.compute_sum())
+print('number of nodes:', my_tree.compute_count())
 
 
-def solve(partial_sol):
-    exam = examine(partial_sol)
-    if exam == ACCEPT:
-        all_solutions.append(partial_sol)
-        print(partial_sol)
-    elif exam != ABANDON:
-        for p in extend(partial_sol):
-            solve(p)
 
-
-def is_solution(candidate_solution):
-    is_valid = False
-    solve([])
-    print(all_solutions)
-    for sol in all_solutions:
-        if sol == candidate_solution:
-            is_valid = True
-    print(f'Is candidate solution: {candidate_solution} valid? {is_valid}')
-
-#solve([])
-
-
-is_solution(['a1', 'd2', 'b3', 'e4', 'c5'])
